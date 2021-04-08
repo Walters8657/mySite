36782aec3c7def62aea42646c344bf1fb8aa0904
Michael Walters <waltersm8@winthrop.edu>
2021-04-07 21:56:47 -0400

    Add resume and mini project pages

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get("/", function(req, res) {
     res.render("pages/index");
 });
 
+app.get("/resume", function(req, res) {
+    res.render("pages/resume");
+});
+
+app.get("/miniProjects", function(req, res) {
+    res.render("pages/miniProjects");
+});
+
+
+
 //Start the server
 app.listen(8080, () => {
     console.log("Listening on port 8080");
