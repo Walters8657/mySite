e96c6a4d2695542d0ff6597c5a1185d8cfe2589a
Michael Walters <waltersm305@gmail.com>
2021-06-23 23:21:01 -0400

    Greatly simplified JS for clicking onimages

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,9 @@
-if(location.pathname == "/") { //Run only on index page
-    var speedoImg1 = document.getElementById("speedoImg1");
-    var speedoImg2 = document.getElementById("speedoImg2");
-    var speedoImg3 = document.getElementById("speedoImg3"); //Gather speedometer preview images 
-    
-    var cahsmImg1 = document.getElementById("cahsmImg1");
-    var cahsmImg2 = document.getElementById("cahsmImg2"); //Gather schedule manager preview images
+if(location.pathname == "/") { //Run only on index page    
+      
+    var images = document.getElementsByClassName("previewImg");
     
     var expandImgDiv = document.getElementById("expandImages"); //Select div that contains big images
-    
-    var expandedSpeedo1 = document.getElementById("expandSpedo1");
-    var expandedSpeedo2 = document.getElementById("expandSpedo2");
-    var expandedSpeedo3 = document.getElementById("expandSpedo3"); //Gather large speedometer images
-    
-    var expandScheduler1 = document.getElementById("expandScheduler1");
-    var expandScheduler2 = document.getElementById("expandScheduler2"); //Gather large schedule manager images
-    
-    var images = [speedoImg1, speedoImg2, speedoImg3, cahsmImg1, cahsmImg2]; //Stores the actual image elements to click on
-    var expandedImages = [expandSpedo1, expandSpedo2, expandSpedo3, expandScheduler1, expandScheduler2]; //Stores the actual expanded image elements   
+    var expandedImages = document.getElementsByClassName("expandedImg"); //Stores the actual expanded image elements
     
     function showProjectImg(imgName) { //Shows container div with darker blured background as well as the img with the passed in ID
         expandImgDiv.classList.add('showExpandedImgDiv');
