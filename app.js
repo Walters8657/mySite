ef6ece57f7a798508b61c7de9bc5ad75bf214f7f
Michael Walters <66094709+Walters8657@users.noreply.github.com>
2022-06-21 18:56:00 -0400

    Rick Roll Added

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/resume", function(req, res) {
     res.render("pages/resume");
 });
 
+app.get("/intro", function(req, res) {
+    res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+})
+
 //Start the server
 app.listen(8080, () => {
     console.log("Listening on port 8080");
