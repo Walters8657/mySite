d5059f952e00e91b3a6704fd6dbdec5c8116d418
Michael Walters <66094709+Walters8657@users.noreply.github.com>
2022-10-01 17:11:17 -0400

    Basic read more read less

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+function showMoreCMS() {
+    document.getElementById("cmsReadMoreContent").className = "";
+    document.getElementById("cmsReadMore").className = "hideElement";
+}
+
+function showLessCMS() {
+    document.getElementById("cmsReadMoreContent").className = "hideElement";
+    document.getElementById("cmsReadMore").className = "";
+}
+
+function showMoreSpeedo() {
+    document.getElementById("speedoReadMoreContent").className = "";
+    document.getElementById("speedoReadMore").className = "hideElement";
+}
+
+function showLessSpeedo() {
+    document.getElementById("speedoReadMoreContent").className = "hideElement";
+    document.getElementById("speedoReadMore").className = "";
+}
