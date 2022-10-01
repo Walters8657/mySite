91862ec0d59b585afbee3dd1ad1db9ed56772eb9
Michael Walters <66094709+Walters8657@users.noreply.github.com>
2022-10-01 14:33:54 -0400

    Footer styling

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-//Gets the current pages nav link and underlines it
-currentLink = document.querySelectorAll('a[href="'+document.location.pathname+'"]');
-currentLink.forEach(function(link) {
-    link.className += ' navActivePage';
-});
