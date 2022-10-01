1ada92bf8cf2d2731643ce128793a3d608a36010
Michael Walters <66094709+Walters8657@users.noreply.github.com>
2022-10-01 19:28:36 -0400

    Complete redesign (#5)
    * Cleans up un needed code
    
    * More code cleanup
    
    * removes unused css file
    
    * Rough redesign of index
    
    * added breakpoint to know where to change design
    
    * Updates contact info
    
    * Updates contact info
    
    * Smaller index images
    
    * Rick Roll Added
    
    * Complete css start over
    
    * Line heights / remove nav partial
    
    * New baseline index page
    
    * Footer styling
    
    * CMS description
    
    * Speedometer description
    
    * Basic read more read less
    
    * Class to center text
    
    * Initial slide toggle, jumpy behavior
    
    * Removes comment
    
    * Removes on click functions from projects
    
    * Adds cursor hover
    
    * Spacing tweaks
    
    * Adds resume + hider spans
    
    * "Page" linking done
    
    * Resume update
    
    * File Cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,29 @@
-if(location.pathname == "/") { //Run only on index page    
-      
-    var images = document.getElementsByClassName("previewImg");
-    
-    var expandImgDiv = document.getElementById("expandImages"); //Select div that contains big images
-    var expandedImages = document.getElementsByClassName("expandedImg"); //Stores the actual expanded image elements
-    
-    function showProjectImg(imgName) { //Shows container div with darker blured background as well as the img with the passed in ID
-        expandImgDiv.classList.add('showExpandedImgDiv');
-        for (var i = 0; i < images.length; i++) {
-            if(imgName == images[i].id) {
-                expandedImages[i].classList.add('showExpandedImg'); //Show clicked on image
-            }
-        };
-    };
-    
-    function hideProjectImages() { //Removes the classes that display larger images
-        expandImgDiv.classList.remove('showExpandedImgDiv');
-        for (var i = 0; i < images.length; i++) {
-            expandedImages[i].classList.remove('showExpandedImg'); //Hide expanded image
-        };
-    };
-}
+$("#cmsReadMore").click(function() {
+    $("#cmsReadMore").hide();
+    $("#cmsReadMoreContent").slideToggle();
+})
 
-//Gets the current pages nav link and underlines it
-currentLink = document.querySelectorAll('a[href="'+document.location.pathname+'"]');
-currentLink.forEach(function(link) {
-    link.className += ' navActivePage';
-});
+$("#cmsReadLess").click(function() {
+    $("#cmsReadMore").show();
+    $("#cmsReadMoreContent").slideToggle();
+})
 
+$("#speedoReadMore").click(function() {
+    $("#speedoReadMore").hide();
+    $("#speedoReadMoreContent").slideToggle();
+})
 
+$("#speedoReadLess").click(function() {
+    $("#speedoReadMore").show();
+    $("#speedoReadMoreContent").slideToggle();
+})
 
+$("#homeLink").click(function() {
+    $("#showcaseHider").show();
+    $("#resumeHider").hide();
+})
+
+$("#resumeLink").click(function() {
+    $("#resumeHider").show();
+    $("#showcaseHider").hide();
+})
